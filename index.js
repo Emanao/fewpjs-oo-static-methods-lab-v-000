6bd7ea406f16805c0b7130ffd29b355b66954884
Eva Navarro <emanao@me.com>
2020-07-22 00:46:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class Formatter {
   //add static methods here
   static capitalize(string){
-    return string.charAt(0).toUpperCase();
+    return string.replace(/^[a-z]/,string.charAt(0).toUpperCase());
   }
 }
